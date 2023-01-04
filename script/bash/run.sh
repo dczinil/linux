bd0b6a12ba6c12a472a934de408d12b356d314f1
tython.kraken.ubu <dczinil@gmail.com>
2023-01-04 11:25:51 -0600

    go v1.19

--- script/bash/run.sh
+++ script/bash/run.sh
@@ -85,7 +85,7 @@ echo "#########################"
 echo "##   Install Golang    ##"
 echo "#########################"
 cd /tmp/installtar
-wget https://dl.google.com/go/go1.13.5.linux-amd64.tar.gz
+wget https://dl.google.com/go/go1.19.4.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.4.linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
 echo "export GOPATH=$HOME/go" >> ~/.bashrc
