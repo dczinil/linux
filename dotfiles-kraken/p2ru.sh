55f7c8dae3ecd743e9336a827693044854b3a0f7
dczinil.tython <dczinil@gmail.com>
2023-01-29 18:06:08 -0600

    files from test

---
+++ dotfiles-kraken/p2ru.sh
@@ -0,0 +1,16 @@
+if [ -d /etc/profile.d ]; then
+    for i in /etc/profile.d/custom.sh; do
+    if [ -r  ]; then
+            . 
+        fi
+    done
+    unset i
+fi
+if [ -d /etc/profile.d ]; then
+    for i in /etc/profile.d/custom.sh; do
+    if [ -r  ]; then
+            . 
+        fi
+    done
+    unset i
+fi
