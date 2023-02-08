07cf10620456ae4390284d366cd8dc60fb169614
dczinil.tython <dczinil@gmail.com>
2023-02-08 14:50:40 -0600

    debug Virtualbox install

--- dotfiles-kraken/dnf.sh
+++ dotfiles-kraken/dnf.sh
@@ -3,35 +3,47 @@
 
 sudo dnf check
 sudo dnf update -y
-sudo dnf install exa nmap gh ncli git htop vim tree curl wget xclip openssh-server ca-certificates gnupg vagrant podman buildah skopeo python3-pip 2>/dev/null
-
+sudo dnf install exa nmap packer gh git htop vim tree curl wget xclip openssl openssh-server ca-certificates gnupg vagrant vagrant-libvirt podman buildah skopeo python3-pip 2>/dev/null
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+#Virtualbox
+src='https://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo'
+dst='/etc/yum.repos.d/virtualbox.repo'
+sudo curl ${src} > sudo ${dst}
+dnf check-update
+sudo dnf install VirtualBox-*
+sudo dnf install -y virtualbox-guest-additions.x86_64
+sudo akmods --kernels 6.1.9-200.fc37.x86_64 && sudo systemctl restart vboxdrv.service
+sudo usermod -aG vboxsf $USER
+sudo usermod -aG vboxusers $USER
+groups $USER | grep -i vbo
+##Vagrant autocomplet
+vagrant autocomplete install --bash
+cat ~/.bashrc | tail -4
+#Pro - Golang - Fonts
 mkdir -p $HOME/{pro,.fonts/{Droid-Sans-Mono,Inconsolata,Source-Code-Pro},go/{bin,pkg,src}}
-
+#SSH
 echo "SSH Configuration"
 echo "Warning... An SSH paaword will be generated with a specific name and revealing its utility."
 echo "The flags "-q" make it silent and "-P" make it not use a phrase."
 ssh-keygen -t ed25519 -C "$GIT_AUTHOR_EMAIL" -f $HOME/.ssh/id_github -q -P "$SSH_PASSPHRASE"
-
+#Git_SSH
 echo "Git Configuration"
 git config --global user.name "$GIT_AUTHOR_NAME"
 git config --global user.email "$GIT_AUTHOR_EMAIL"
 git config --global core.editor vim
 
-
-
-
 cd $HOME/pro/
-git clone https://github.com/dczinil/linux.git
-git clone https://github.com/dczinil/pages.git
-git clone https://github.com/dczinil/vagrant.git
-git clone https://github.com/dczinil/ansible.git
-git clone https://github.com/dczinil/python.git
-git clone https://github.com/dczinil/dczinil.git
-git clone https://github.com/dczinil/git.git
-git clone https://github.com/dczinil/docker.git
-git clone https://github.com/dczinil/dczinil.github.io.git
-git clone https://github.com/jwiegley/git-scripts.git
-git clone https://github.com/arialdomartini/oh-my-git.git ~/.oh-my-git && echo source ~/.oh-my-git/prompt.sh >> ~/.bashrc
+git clone git@github.com:dczinil/linux.git
+git clone git@github.com:dczinil/pages.git
+git clone git@github.com:dczinil/vagrant.git
+git clone git@github.com:dczinil/ansible.git
+git clone git@github.com:dczinil/python.git
+git clone git@github.com:dczinil/dczinil.git
+git clone git@github.com:dczinil/dczinil/git.git
+git clone git@github.com:dczinil/docker.git
+git clone git@github.com:dczinil/dczinil.github.io.git
+git clone git@github.com:jwiegley/git-scripts.git
+git clone git@github.com:arialdomartini/oh-my-git.git ~/.oh-my-git && echo source ~/.oh-my-git/prompt.sh >> ~/.bashrc
 #Cargo
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 cargo install vivid
