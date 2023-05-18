4fbebc27213000a5883b872abe73b539589ecd51
Replit user <>
2023-05-18 18:00:57 +0000

    debug testing 200

--- script/python/listdir.py
+++ script/python/listdir.py
@@ -1,15 +1,14 @@
 import os
-name_of_dir = 'dir_path'
+name_of_dir = '/data/data/com.termux/files/home/'
 
-list_of_files = filter(labda x: os.path.isfile
-											(os.path.join(name_of_dir, x)),
-											 os.listdir(dir_name))
+list_of_files = filter(lambda x: os.path.isfile
+                (os.path.join(name_of_dir, x)),
+                os.listdir(name_of_dir) )
 list_of_files = sorted(list_of_files,
-											key = lambda x: os.stat
-											(os.path.join.(name_of_dir, x)).st_size)
+                key = lambda x: os.stat
+                (os.path.join(name_of_dir, x)).st_size)
 
 for name_of_file in list_of_files:
-	path_of_file = os.path.join(name_of_dir, name_of_file)
-	size_of_file = os.stat(path_of_file).st_size
-	print(size_of_file, ' -->', name_of_file)
-	
+        path_of_file = os.path.join(name_of_dir, name_of_file)
+        size_of_file = os.stat(path_of_file).st_size
+        print(size_of_file, ' -->', name_of_file)
