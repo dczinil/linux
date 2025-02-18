dac003f789cc79302468e2800fdfbdcec63a4ff2
dczinil.tython <dczinil@gmail.com>
2025-02-17 23:49:25 -0600

    node root

--- dotfiles-kraken/dnf.sh
+++ dotfiles-kraken/dnf.sh
@@ -57,8 +57,13 @@ git config --global core.editor vim
 #<!-------------------------!>#
 #     Install NVM and TLS
 #<!-------------------------!>#
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 nvm install --tls
+VNODEJS=$(node --version)
+sudo cp -r $HOME/.nvm/versions/node/$VNODEJS/usr/local/nodejs
+sudo ln -sf /usr/local/nodejs/bin/node /usr/local/bin/node
+sudo ln -sf /usr/local/nodejs/bin/npm /usr/local/bin/npm
+sudo ln -sf /usr/local/nodejs/bin/npx /usr/local/bin/npx
 #<!-------------------------!>#
 #     Download Repo
 #<!-------------------------!>#
